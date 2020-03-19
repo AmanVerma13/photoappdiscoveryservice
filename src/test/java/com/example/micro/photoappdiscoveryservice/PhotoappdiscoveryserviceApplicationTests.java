6abecfbeaa7caf21b5ea30caf816b94fbbe72d1e
Aman Verma <53325551+AmanVm13@users.noreply.github.com>
2020-03-19 12:13:02 +0530

    Initial commit

---
+++ src/test/java/com/example/micro/photoappdiscoveryservice/PhotoappdiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micro.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappdiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
